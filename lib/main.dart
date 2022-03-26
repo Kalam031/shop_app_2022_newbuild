1879bf1b44aac51e8c053b44ccbbc5cbc140f391
Kalam <Kalam@DESKTOP-2URI2JC>
2022-03-26 17:11:02 +0600

    integrate provider and view data in product overview screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+
+import './providers/products.dart';
+import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 
 void main() {
@@ -12,30 +15,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        colorScheme: ColorScheme(
-          brightness: Brightness.light,
-          primary: Colors.purple,
-          onPrimary: Colors.purpleAccent,
-          secondary: Colors.deepOrange,
-          onSecondary: Colors.orange,
-          error: Colors.red,
-          onError: Colors.redAccent,
-          background: Colors.white,
-          onBackground: Colors.white10,
-          surface: Colors.white,
-          onSurface: Colors.white10,
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          colorScheme: ColorScheme(
+            brightness: Brightness.light,
+            primary: Colors.purple,
+            onPrimary: Colors.purpleAccent,
+            secondary: Colors.deepOrange,
+            onSecondary: Colors.orange,
+            error: Colors.red,
+            onError: Colors.redAccent,
+            background: Colors.white,
+            onBackground: Colors.white10,
+            surface: Colors.white,
+            onSurface: Colors.white10,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routename: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routename: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
