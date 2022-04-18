158a06270cbced73a1aabdec24c349d9bb84e494
Kalam <Kalam@DESKTOP-2URI2JC>
2022-04-18 19:44:06 +0600

    log in and get response back

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,9 +8,10 @@ class Auth with ChangeNotifier {
   late DateTime _expiryDate;
   late String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC_Tz1pT6rl2NspXQBwmN25iP_m4y0jKlY';
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyC_Tz1pT6rl2NspXQBwmN25iP_m4y0jKlY';
 
     final response = await http.post(
       Uri.parse(url),
@@ -21,5 +22,13 @@ class Auth with ChangeNotifier {
       }),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
